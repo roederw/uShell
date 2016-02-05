2b0acc6c4d11097de6e1a6507303d1a914deef86
Quinn Perfetto <thequinnftw@gmail.com>
2016-02-05 11:10:12 -0500

    cleaned up some API code

--- uvaapi.py
+++ uvaapi.py
@@ -12,8 +12,9 @@ A wrapper for functions which interface with www.uhunt.felix-halim.net/api
 '''
 
 # Make a get request to a given endpoint in the API
-def api_get(endpoint):
-    return requests.get("http://uhunt.felix-halim.net/api/" + endpoint).text.strip()
+def api_get(*args):
+    fmt = "/".join(map(str, args))
+    return requests.get("http://uhunt.felix-halim.net/api/" + fmt).text.strip()
 
 # Build a problem dictionary, will only actually make request once
 # every 24 hours
@@ -26,45 +27,52 @@ def fetch_problems():
         jsoncache.put("problems", nd, 24)
     return jsoncache.get("problems")
 
-problems = fetch_problems()
-
 # Convert a UVa username to a uid (needed for other endpoints)
 def get_uid(username):
-    return api_get("uname2uid/" + username)
+    return api_get("uname2uid", username)
 
 # Convert a Uva problem number to a pid (needed for other endpoints)
 def get_pid(problem_num):
-    return json.loads(api_get("p/num/" + problem_num))['pid']
+    return json.loads(api_get("p/num", problem_num))['pid']
 
 def get_problem_name(problem_num):
-    return json.loads(api_get("p/num/" + problem_num))['title']
+    return json.loads(api_get("p/num", problem_num))['title']
 
 # Takes a submission and converts it to a readable format
-def _clean_sub(sub):
-    cleaned = {
-        "problem": problems[str(sub[1])],
-        "verdict": constants.verdicts[sub[2]],
-        "runtime": str(sub[3]) + "ms",
-        "language": constants.language[sub[5]],
-        "submission_id": sub[0]
-    }
-    return cleaned
+
+def returns_submissions(func):
+    problems = fetch_problems()
+    def clean_sub(sub):
+        cleaned = {
+            "problem": problems[str(sub[1])],
+            "verdict": constants.verdicts[sub[2]],
+            "runtime": str(sub[3]) + "ms",
+            "language": constants.language[sub[5]],
+            "submission_id": sub[0]
+        }
+        return cleaned
+
+    def wrap(*args):
+        ret = func(*args)
+        return map(clean_sub, ret)
+    return wrap
 
 # Retrieves the last n submissions for a given user
+@returns_submissions
 def submissions(uid, n):
-    subs = reversed(json.loads(api_get("subs-user-last/" + uid + "/"+str(n)))["subs"])
-    return map(_clean_sub, subs)
+    return reversed(json.loads(api_get("subs-user-last", uid, n))["subs"])
 
-def leaderboard(problem_num, n):
-    leaders = json.loads(api_get("p/rank/" + str(get_pid(problem_num)) + "/1/" + str(n)))
-    return leaders
-
+@returns_submissions
 def user_submissions(user, n = 10):
     uid = get_uid(user)
-    subs = reversed(json.loads(api_get("subs-user-last/" + uid + "/" + str(n)))["subs"])
-    return map(_clean_sub, subs)
+    return reversed(json.loads(api_get("subs-user-last", uid, n))["subs"])
 
+@returns_submissions
 def user_submissions_problem(user, problem_num):
     uid = get_uid(user)
-    subs = reversed(json.loads(api_get("subs-pids/" + uid + "/" + str(get_pid(problem_num)) + "/0"))[uid]["subs"])
-    return map(_clean_sub, subs)
+    resp = json.loads(api_get("subs-pids", uid, get_pid(problem_num), 0))
+    return reverse(resp[uid]["subs"])
+
+def leaderboard(problem_num, n):
+    leaders = json.loads(api_get("p/rank", get_pid(problem_num), 1, n))
+    return leaders
