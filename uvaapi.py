6277386d6bdaf2840b1e78fc09ca1ce55d854e08
Quinn Perfetto <thequinnftw@gmail.com>
2016-02-05 11:14:35 -0500

    return json parsed by default

--- uvaapi.py
+++ uvaapi.py
@@ -14,13 +14,14 @@ A wrapper for functions which interface with www.uhunt.felix-halim.net/api
 # Make a get request to a given endpoint in the API
 def api_get(*args):
     fmt = "/".join(map(str, args))
-    return requests.get("http://uhunt.felix-halim.net/api/" + fmt).text.strip()
+    resp = requests.get("http://uhunt.felix-halim.net/api/" + fmt).text.strip()
+    return json.loads(resp)
 
 # Build a problem dictionary, will only actually make request once
 # every 24 hours
 def fetch_problems():
     if not jsoncache.get("problems"):
-        p = json.loads(api_get("p"))
+        p = api_get("p")
         nd = {}
         for i in p:
             nd[i[0]] = {"problem_number": i[1], "title": i[2]}
@@ -33,10 +34,10 @@ def get_uid(username):
 
 # Convert a Uva problem number to a pid (needed for other endpoints)
 def get_pid(problem_num):
-    return json.loads(api_get("p/num", problem_num))['pid']
+    return api_get("p/num", problem_num)['pid']
 
 def get_problem_name(problem_num):
-    return json.loads(api_get("p/num", problem_num))['title']
+    return api_get("p/num", problem_num)['title']
 
 # Takes a submission and converts it to a readable format
 
@@ -60,19 +61,19 @@ def returns_submissions(func):
 # Retrieves the last n submissions for a given user
 @returns_submissions
 def submissions(uid, n):
-    return reversed(json.loads(api_get("subs-user-last", uid, n))["subs"])
+    return reversed(api_get("subs-user-last", uid, n)["subs"])
 
 @returns_submissions
 def user_submissions(user, n = 10):
     uid = get_uid(user)
-    return reversed(json.loads(api_get("subs-user-last", uid, n))["subs"])
+    return reversed(api_get("subs-user-last", uid, n)["subs"])
 
 @returns_submissions
 def user_submissions_problem(user, problem_num):
     uid = get_uid(user)
-    resp = json.loads(api_get("subs-pids", uid, get_pid(problem_num), 0))
+    resp = api_get("subs-pids", uid, get_pid(problem_num), 0)
     return reverse(resp[uid]["subs"])
 
 def leaderboard(problem_num, n):
-    leaders = json.loads(api_get("p/rank", get_pid(problem_num), 1, n))
+    leaders = api_get("p/rank", get_pid(problem_num), 1, n)
     return leaders
